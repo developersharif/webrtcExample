b3d20afe37a34ea614a2b2c9bf20198a121d5f2d
developersharif <developersharif@yahoo.com>
2023-12-20 11:43:44 +0600

    controls added

--- main.js
+++ main.js
@@ -1,16 +1,14 @@
 import "./style.css"
 //STUN,TURN server info can be pass to RTCPeerConnection object constructor
-const iceServers = [
-  { urls: 'stun:stun.l.google.com:19302' },
-  // {
-  //   urls: 'turn:turn-server.com:3478',
-  //   username: 'username',
-  //   credential: 'password'
-  // }
-];
-
+const peerConfiguration = {}
+async function getIcerServers() {
+  // const response = await fetch("https://realbrain.metered.live/api/v1/turn/credentials?apiKey=ff29501ba025c8ffea1ec276bffc4f8dd96f");
+  // const iceServers = await response.json();
+  // peerConfiguration.iceServers = iceServers
+};
+getIcerServers();
 //Peer Connection Installation
-const peerConnection = new RTCPeerConnection({iceServers});
+const peerConnection = new RTCPeerConnection(peerConfiguration);
 let localStream;
 let remoteStream;
 
@@ -18,17 +16,20 @@ let remoteStream;
 const localVideo = document.getElementById("localVideo");
 //Remote User Stream
 const remoteVideo = document.getElementById("remoteVideo");
-
 //SDP Description Buttons
 const createOfferButton = document.getElementById("create_offer");
 const createAnswerButton = document.getElementById("create_answer");
 const addAnswerButton = document.getElementById("add_answer");
 const sdpOffer = document.getElementById("sdp_offer");
 const sdpAnswer = document.getElementById("sdp_answer");
-
+const muteButton = document.getElementById("muteButton");
+const videoButton = document.getElementById("videoButton");
+const menu = document.getElementById("menu");
+const dashboard = document.getElementById("dashboard");
 //default options
-const localStreamConstrains = {video:true,audio:false}
-
+const localStreamConstrains = {video:true,audio:true}
+let isAudioMuted = true;
+let isVideoEnabled = true;
 init();
 //Initialize First Step
 async function init(){
@@ -76,7 +77,23 @@ async function addAnswer(){
     peerConnection.setRemoteDescription(answer)
   }
 }
+muteButton.onclick  = () => {
+  localStream.getAudioTracks().forEach(track => {
+      track.enabled = !isAudioMuted;
+  });
+  isAudioMuted = !isAudioMuted;
+  muteButton.innerHTML = isAudioMuted ? '<i class="fa-solid fa-microphone"></i>' : '<i class="fa-solid fa-microphone-slash"></i>';
+};
 
+videoButton.onclick =  () => {
+  localStream.getVideoTracks().forEach(track => {
+      track.enabled = isVideoEnabled = !isVideoEnabled;
+  });
+  videoButton.innerHTML = isVideoEnabled ? '<i class="fa-solid fa-video"></i>' : '<i class="fa-solid fa-video-slash"></i>';
+};
+menu.onclick = ()=>{
+  dashboard.classList.toggle('hidden');
+}
 //Event handlers
 createOfferButton.onclick = createOffer
 createAnswerButton.onclick = createAnswer
